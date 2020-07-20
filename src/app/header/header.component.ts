a33d0deb2fe7d3986e8df5e8f5e4dcfd676ab78e
mrChernicharo <felipe.chernicharo@gmail.com>
2020-07-20 03:02:55 -0300

    alternar página com ngIf e event bind

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
 	selector: 'app-header',
@@ -8,6 +8,11 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
   title = 'header';
   collapsed = true;
+  @Output() featureSelected = new EventEmitter<string>();
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
 }
 
 
