4718c2b505d21fffca766a06f8d5de6c6b44aade
mrChernicharo <felipe.chernicharo@gmail.com>
2020-07-28 18:55:59 -0300

    access NgForm with ViewChild + form setValue()

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 
 import { Ingredient } from '../../shared/ingredient.model'
 import { ShoppingListService } from '../shopping-list.service';
@@ -11,10 +11,11 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f', { static: false }) slForm: NgForm
   subscription: Subscription;
   editMode = false;
   editedItemIndex: number;
-
+  editedItem: Ingredient;
 
   constructor(private slService: ShoppingListService) { }
 
@@ -22,7 +23,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.subscription = this.slService.startedEditing.subscribe((index: number) => {
       this.editMode = true;
       this.editedItemIndex = index;
-
+      this.editedItem = this.slService.getIngredient(index);
+      this.slForm.setValue({
+        name : this.editedItem.name,
+        amount: this.editedItem.amount
+      })
     });
   }
 
