6baed0c400387826965fe54bfc02548d81b69c76
mrChernicharo <felipe.chernicharo@gmail.com>
2020-08-03 16:07:11 -0300

    autoLogout X manualLogout & tokenExpiration

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -19,6 +19,7 @@ export interface AuthResponseData {
 export class AuthService {
   private apiKey = 'AIzaSyDZqT-XyNwS91VLRESjKCKK-1B20gHKC5g'
   public user = new BehaviorSubject<User>(null);
+  private tokenExpirationTimer: any;
 
   constructor(
     private http: HttpClient,
@@ -75,12 +76,26 @@ export class AuthService {
 
     if (loadedUser.token) {
       this.user.next(loadedUser);
+      const expirationTime = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+      this.autoLogout(expirationTime);
     }
   }
 
   logout() {
     this.user.next(null);
-    this.router.navigate(['/auth'])
+    this.router.navigate(['/auth']);
+    localStorage.removeItem('userData');
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
+  }
+
+  autoLogout(expirationTime: number) {
+    console.log(`token expiration ${expirationTime}ms`);
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout()
+    }, expirationTime); // <== set hardcoded ms to test userData expiration
   }
 
   private handleAuthentication(
@@ -96,6 +111,7 @@ export class AuthService {
       expirationDate
       );
     this.user.next(user);
+    this.autoLogout(expiresIn * 1000);
     localStorage.setItem('userData', JSON.stringify(user));
   }
 
