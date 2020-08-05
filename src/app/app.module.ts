5e3e0f3f315401b462547809847d55ea38eb5b4c
mrChernicharo <felipe.chernicharo@gmail.com>
2020-08-05 00:47:19 -0300

    seting the store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { CoreModule } from './core.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { StoreModule } from '@ngrx/store';
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 @NgModule({
   declarations: [
@@ -21,6 +23,7 @@ import { HeaderComponent } from './header/header.component';
     AppRoutingModule,
     SharedModule,
     CoreModule,
+    StoreModule.forRoot({ shoppingList: shoppingListReducer })
   ],
   bootstrap: [AppComponent]
 })
